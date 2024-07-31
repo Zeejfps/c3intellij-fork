4c2f14fc8fed79f24033d68c231fedd89640abac
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-07-31 17:42:07 -0500

    Saving the current form

--- src/main/java/org/c3lang/intellij/C3ModuleSettingsForm.java
+++ src/main/java/org/c3lang/intellij/C3ModuleSettingsForm.java
@@ -1,12 +1,16 @@
 package org.c3lang.intellij;
 
+import com.intellij.ui.ComboboxWithBrowseButton;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.ui.components.JBLabel;
+
 import javax.swing.*;
 
 public class C3ModuleSettingsForm {
     private JPanel mainPanel;
     private JTextField textField1;
-    private JTextField textField2;
-    private JComboBox comboBox1;
+    private ComboboxWithBrowseButton comboboxWithBrowseButton1;
+    private TextFieldWithBrowseButton textFieldWithBrowseButton1;
 
     public JComponent getComponent() {
         return mainPanel;
