7ab93dbed4d75342c863b5e45b4dfcaf789d3026
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-07-31 18:28:56 -0500

    Ok got the form in a good enough place

--- src/main/java/org/c3lang/intellij/C3ModuleSettingsForm.java
+++ src/main/java/org/c3lang/intellij/C3ModuleSettingsForm.java
@@ -1,16 +1,14 @@
 package org.c3lang.intellij;
 
-import com.intellij.ui.ComboboxWithBrowseButton;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.ui.components.JBLabel;
 
 import javax.swing.*;
 
 public class C3ModuleSettingsForm {
     private JPanel mainPanel;
     private JTextField textField1;
-    private ComboboxWithBrowseButton comboboxWithBrowseButton1;
-    private TextFieldWithBrowseButton textFieldWithBrowseButton1;
+    private TextFieldWithBrowseButton comboboxWithBrowseButton1;
+    private TextFieldWithBrowseButton cUsersZeejfLibraryTextFieldWithBrowseButton;
 
     public JComponent getComponent() {
         return mainPanel;
