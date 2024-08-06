721463f2890ac58619fa1b4b3cb1e32c0d8b5ce1
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-08-06 09:01:17 -0500

    Hooked up the compiler field

--- src/main/java/org/c3lang/intellij/projectWizard/C3ProjectGeneratorPeer.java
+++ src/main/java/org/c3lang/intellij/projectWizard/C3ProjectGeneratorPeer.java
@@ -2,18 +2,26 @@ package org.c3lang.intellij.projectWizard;
 
 import com.intellij.ide.util.projectWizard.ModuleNameLocationSettings;
 import com.intellij.ide.util.projectWizard.SettingsStep;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.ui.ValidationInfo;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.ProjectGeneratorPeer;
 import com.intellij.ui.DocumentAdapter;
 import com.intellij.ui.EnumComboBoxModel;
 import com.intellij.ui.components.JBTextField;
+import com.jetbrains.cidr.lang.workspace.CompilerSettingsData;
+import org.c3lang.intellij.C3SettingsState;
+import org.c3lang.intellij.sdk.C3SdkType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -40,6 +48,25 @@ public class C3ProjectGeneratorPeer implements ProjectGeneratorPeer<C3ProjectGen
         });
 
         compilerField = new TextFieldWithBrowseButton();
+        compilerField.addBrowseFolderListener("Select C3 Compiler", "", null, new FileChooserDescriptor(true, false, false, false, false, false)
+        {
+            @Override
+            public boolean isFileSelectable(@Nullable VirtualFile file)
+            {
+                if (file == null) return false;
+                if (!file.exists()) return false;
+                if (file.isDirectory()) return false;
+                if (!Files.isExecutable(file.toNioPath())) return false;
+                return C3SdkType.isSdkHome(file.getParent().getPath());
+            }
+        });
+        compilerField.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
+
+            @Override
+            protected void textChanged(@NotNull DocumentEvent documentEvent) {
+                NotifySettingsChanged();
+            }
+        });
     }
 
     @Override
@@ -52,6 +79,9 @@ public class C3ProjectGeneratorPeer implements ProjectGeneratorPeer<C3ProjectGen
         this.settingsStep = settingsStep;
         settingsStep.addSettingsField("Kind", projectKindComboBox);
         settingsStep.addSettingsField("Compiler", compilerField);
+        
+        C3SettingsState settingsState = C3SettingsState.getInstance();
+        compilerField.setText(settingsState.sdk);
     }
     
     private void NotifySettingsChanged() {
@@ -80,6 +110,11 @@ public class C3ProjectGeneratorPeer implements ProjectGeneratorPeer<C3ProjectGen
         if (!matcher.matches()) {
             return new ValidationInfo("Project name can only have letters, numbers, and _");
         } 
+        
+        Path pathToCompilerExe = Path.of(compilerField.getText());
+        if (!C3SdkType.isSdkHome(pathToCompilerExe.getParent().toString())) {
+            return new ValidationInfo("Project compiler location is not valid");
+        }
         
         return null;
     }
