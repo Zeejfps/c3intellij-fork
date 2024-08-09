3a4268041564f595e6476fbd979bc6f295660e51
Zeejfps@gmail.com <Zeejfps@gmail.com>
2024-08-09 18:24:47 -0500

    Deleted SetprojectName and GetProjectName from ProjectGeneratorSettings

--- src/main/java/org/c3lang/intellij/projectWizard/C3ProjectGeneratorSettings.java
+++ src/main/java/org/c3lang/intellij/projectWizard/C3ProjectGeneratorSettings.java
@@ -2,16 +2,7 @@ package org.c3lang.intellij.projectWizard;
 
 public class C3ProjectGeneratorSettings {
 
-    private String projectName;
     private C3ProjectKind projectKind = C3ProjectKind.Exe;
-    
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-    
-    public String getProjectName() {
-        return projectName;
-    }
     
     public void setProjectKind(C3ProjectKind projectKind) {
         this.projectKind = projectKind;
